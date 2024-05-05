a99a7e232310b7cacd937eb41b0ef7a799fb4258
Juan_F <juan.e.franco@correounivalle.edu.co>
2024-05-05 16:51:25 -0500

    ventana vacia

--- mini_proyecto2/src/App.java
+++ mini_proyecto2/src/App.java
@@ -1,5 +1,23 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.JFrame;
+import java.awt.Container;
+
+public class App extends JFrame
+{
+    Container contenedor;
+    public App()
+    {
+        setTitle("Ferreteria");
+        setSize(500, 300);
+        setVisible(true);
+
+
+
+    }
+    
+    
+    public static void main(String[] args) throws Exception
+    {
+        App app = new App();
+        System.out.println(app);
     }
 }
