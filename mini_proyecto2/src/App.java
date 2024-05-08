0d6e26b3e9cd2dccd1153fb0f84838755369a672
Juan_F <juan.e.franco@correounivalle.edu.co>
2024-05-07 22:34:46 -0500

    Correccion archivo app añadido

---
+++ mini_proyecto2/src/App.java
@@ -0,0 +1,12 @@
+import Windows.Ventana;
+
+public class App 
+{
+    
+    public static void main(String[] args) throws Exception 
+    {
+        Ventana v2 = new Ventana();
+        System.out.println(v2);
+        v2.setVisible(true);
+    }
+}
