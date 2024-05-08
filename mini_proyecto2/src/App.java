5bac41c616ce57d21415ef2a45a55747b1ce626f
Juan_F <juan.e.franco@correounivalle.edu.co>
2024-05-07 22:47:53 -0500

    Correccion parte1

--- mini_proyecto2/src/App.java
+++ mini_proyecto2/src/App.java
@@ -5,6 +5,7 @@ public class App
     
     public static void main(String[] args) throws Exception 
     {
+        //inicia la ventana
         Ventana v2 = new Ventana();
         System.out.println(v2);
         v2.setVisible(true);
